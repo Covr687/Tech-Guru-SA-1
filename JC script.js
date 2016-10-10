09dc8930a17d9381a68f9749647139873a83ece3
Covr687 <christech687@yahoo.com>
2016-10-10 18:27:35 -0500

    Add files via upload

---
+++ JC script.js
@@ -0,0 +1,15 @@
+jQuerry(document).ready(function(){
+    
+   jQuery("menu-trigger").click(function(){
+      
+       jQuery("mainheader").slideToggle(400 function(){
+        jQuery(this).toggleClass("nav-expand").css('display','')   
+           
+       
+       });
+       
+   });
+                                
+    
+    
+});
